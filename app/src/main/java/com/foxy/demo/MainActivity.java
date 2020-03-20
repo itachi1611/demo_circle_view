45fdfd23b08e730cd4ad124b99a008bf90405ef9
foxy <itachi1611@gmail.com>
2020-03-20 21:06:14 +0700

    Initial commit

---
+++ app/src/main/java/com/foxy/demo/MainActivity.java
@@ -0,0 +1,21 @@
+package com.foxy.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void clickThu(View view) {
+        showDiadlog
+    }
+}
